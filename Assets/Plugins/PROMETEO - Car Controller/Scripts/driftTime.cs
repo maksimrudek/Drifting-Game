d5e686ff00a5daff5560dcd55265824d81ad346e
BuildTools <maksim.rudek@gmail.com>
2023-06-07 09:11:59 +0300

    Finish(Under Construction)

--- Assets/Plugins/PROMETEO - Car Controller/Scripts/driftTime.cs
+++ Assets/Plugins/PROMETEO - Car Controller/Scripts/driftTime.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
 using TMPro;
+using System.Collections.Generic;
 
-public class driftTime : MonoBehaviour
+public class DriftTime : MonoBehaviour
 {
     public PrometeoCarController car;
 
@@ -12,23 +13,60 @@ public class driftTime : MonoBehaviour
     public TextMeshProUGUI driftAmountText;
     public TextMeshProUGUI countdownText;
     public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI lapText;
+    public TextMeshProUGUI scoreText;
 
-    private const float ScoreMultiplier = 100f;
+    public GameObject playButton;
+    public GameObject exitButton;
+    public GameObject exitButton1;
+    public GameObject restartButton;
+    public GameObject aboutButton;
+
+    public Transform startingPosition;
+
+    private float ScoreMultiplier = 100f;
     private bool blinkActive;
     private float BlinkDuration = 3f;
     private float blinkTimer;
     private float BlinkInterval = 0.5f;
 
-    private float countdownTimer = 30f;
+    private float countdownTimer = 10f;
     private bool isCountingDown = true;
 
-    void Start()
+    private int lap = 1;
+
+    private bool lapTriggered = false;
+    private float totalScore = 0f;
+    private List<float> driftScores = new List<float>();
+    private float gameDriftScore = 0f;
+
+    private bool gameStarted = false;
+
+    private Vector3 originalCarPosition;
+    private Quaternion originalCarRotation;
+
+    private void Start()
     {
+        playButton.SetActive(true);
+        exitButton.SetActive(true);
+        aboutButton.SetActive(true);
+        exitButton1.SetActive(false);
+        restartButton.SetActive(false);
         gameOverText.gameObject.SetActive(false);
+        scoreText.gameObject.SetActive(false);
+        lapText.text = "Lap: " + lap;
+
+        originalCarPosition = car.transform.position;
+        originalCarRotation = car.transform.rotation;
+
+        Time.timeScale = 0f;
     }
 
-    void Update()
+    private void Update()
     {
+        if (!gameStarted)
+            return;
+
         if (isCountingDown)
         {
             countdownTimer -= Time.deltaTime;
@@ -79,17 +117,33 @@ public class driftTime : MonoBehaviour
         }
     }
 
-    void UpdateDriftAmountText()
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!gameStarted)
+            return;
+
+        if (other.CompareTag("LapTrigger") && !lapTriggered)
+        {
+            lapTriggered = true;
+            lap++;
+            lapText.text = "Lap: " + lap;
+            driftScores.Add(gameDriftScore);
+            gameDriftScore = 0f; 
+        }
+    }
+
+    private void UpdateDriftAmountText()
     {
         if (driftAmountText != null)
         {
             driftAmountText.text = Mathf.RoundToInt(driftAmount).ToString();
             driftAmountText.gameObject.SetActive(true);
             StartBlinking();
+            gameDriftScore = driftAmount;
         }
     }
 
-    void HideDriftAmountText()
+    private void HideDriftAmountText()
     {
         if (driftAmountText != null)
         {
@@ -97,13 +151,13 @@ public class driftTime : MonoBehaviour
         }
     }
 
-    void StartBlinking()
+    private void StartBlinking()
     {
         blinkActive = true;
         blinkTimer = BlinkDuration;
     }
 
-    void UpdateCountdownText()
+    private void UpdateCountdownText()
     {
         if (countdownText != null)
         {
@@ -111,9 +165,72 @@ public class driftTime : MonoBehaviour
         }
     }
 
-    void GameOver()
+    public void PlayGame()
+    {
+        playButton.SetActive(false);
+        exitButton.SetActive(false);
+        restartButton.SetActive(false);
+        aboutButton.SetActive(false);
+
+        Time.timeScale = 1f;
+
+        gameStarted = true;
+    }
+
+    private void GameOver()
     {
         gameOverText.gameObject.SetActive(true);
-        Time.timeScale = 0f; // Stop the game
+        Time.timeScale = 0f;
+        lapText.gameObject.SetActive(false);
+
+        driftScores.Add(gameDriftScore);
+
+       
+        foreach (float score in driftScores)
+        {
+            totalScore += score;
+        }
+
+        scoreText.gameObject.SetActive(true);
+        int roundedScore = Mathf.RoundToInt(totalScore * lap);
+        scoreText.text = "Last Highest Score: " + roundedScore.ToString();
+
+        
+        restartButton.SetActive(true);
+        exitButton1.SetActive(true);
     }
+
+    public void RestartGame()
+    {
+        lap = 1;
+        lapText.text = "Lap: " + lap;
+        gameStarted = true;
+        gameOverText.gameObject.SetActive(false);
+        scoreText.gameObject.SetActive(false);
+        lapText.gameObject.SetActive(true);
+        Time.timeScale = 1f;
+        totalScore = 0f;
+        driftScores.Clear();
+        gameDriftScore = 0f;
+
+        car.transform.position = originalCarPosition;
+        car.transform.rotation = originalCarRotation;
+
+        countdownTimer = 10f;
+        isCountingDown = true;
+
+        driftAmount = 0f;
+        driftingTime = 0f;
+        gameDriftScore = 0f;
+
+        restartButton.SetActive(false);
+        exitButton1.SetActive(false);
+    }
+
+    public void OpenGitHubLink()
+    {
+        string githubURL = "https://github.com/maksimrudek/Drifting-Game";
+        Application.OpenURL(githubURL);
+    }
+
 }
